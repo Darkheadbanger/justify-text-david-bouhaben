cc075f2076cda734f6470c9b5950a3d606947b1b
David_Bouhaben <davidbouhaben@yahoo.co.id>
2025-10-19 20:33:32 +0200

    Refactor app.ts: remove unnecessary comments for clarity

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,9 @@ import justifRoutes from "./routes/justify.route.js";
 
 const app: express.Express = express();
 
-// Middlewares pour parser le body
-// Augmenter la limite pour permettre de gros textes (pour tester le rate limiting)
 app.use(express.json({ limit: "10mb" }));
 app.use(express.text({ type: "text/plain", limit: "10mb" }));
 
-// Routes de l'API
 app.use("/api", tokenRoutes);
 app.use("/api", justifRoutes);
 
