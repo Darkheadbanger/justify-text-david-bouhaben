e831482c0c78b470f3fe5f839cb76474809414a1
David_Bouhaben <davidbouhaben@yahoo.co.id>
2025-10-19 22:05:14 +0200

    Refactor justify.route.ts: extract justify path to a constant for improved readability

--- src/routes/justify.route.ts
+++ src/routes/justify.route.ts
@@ -3,7 +3,7 @@ import { justify } from "../controllers/justify.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router: express.Router = express.Router();
-
-router.post("/justify", authenticate, justify);
+const justifyPath: string = "/justify";
+router.post(justifyPath, authenticate, justify);
 
 export default router;
