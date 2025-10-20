ed404fcd2b4f562f22dc363de796c477203dc89d
David_Bouhaben <davidbouhaben@yahoo.co.id>
2025-10-20 13:05:29 +0200

    fix: remove leading slash from apiLink in justify and tokens routes

--- src/routes/justify.route.ts
+++ src/routes/justify.route.ts
@@ -3,7 +3,7 @@ import { justify } from "../controllers/justify.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router: express.Router = express.Router();
-const apiLink: string = "/api";
+const apiLink: string = "api";
 
 const justifyPath: string = "/justify";
 router.post(apiLink + justifyPath, authenticate, justify);
--- src/routes/tokens.route.ts
+++ src/routes/tokens.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { generateTokenController } from "../controllers/token.controller.js";
 
 const router: express.Router = express.Router();
-const apiLink: string = "/api";
+const apiLink: string = "api";
 
 const tokenPath: string = "/token";
 router.post(apiLink + tokenPath, generateTokenController);
